70e0b98d27dbf0e4ef2c6beaf2fe26243f877bd1
APOLLO <zlchanger@gmail.com>
2018-07-04 09:34:57 +0800

    项目创建

---
+++ src/main/java/com/fengding/embroidery/embroidery/EmbroideryBackstageApplication.java
@@ -0,0 +1,12 @@
+package com.fengding.embroidery.embroidery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbroideryBackstageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbroideryBackstageApplication.class, args);
+	}
+}
